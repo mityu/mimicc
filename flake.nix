3128a8d9a326208eeb2e7558b6c1c64e71d506de
mityu <mityu.mail@gmail.com>
2025-10-16 20:33:29 +0900

    feat: Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          gcc
+          glibc.static
+        ];
+      };
+    };
+}
