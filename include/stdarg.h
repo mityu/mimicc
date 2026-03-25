7b47daa0829058dd70fd464cfe1dd1811ee418ad
mityu <mityu.mail@gmail.com>
2026-03-25 17:43:48 +0900

    feat: Implement va_copy()

--- include/stdarg.h
+++ include/stdarg.h
@@ -1,8 +1,12 @@
 #ifndef __MIMICC_STDARG_H
 #define __MIMICC_STDARG_H
 
+#include <string.h>
+
 #define va_start(ap, fmt) __builtin_va_start(ap, fmt)
 #define va_end(ap)
+#define va_copy(dst, src) __builtin_va_copy(dst, src)
+#define __builtin_va_copy(dst, src) (memcpy((dst), (src), sizeof(va_list)))
 
 typedef struct {
     int gp_offset;
